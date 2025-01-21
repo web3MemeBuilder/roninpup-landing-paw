3325a14dccd16020fa1d81bd606219380d62a0f7
RicardoMuchacho <rmuchacho@binwus.com>
2025-01-21 13:15:34 +0100

    Adjustments, logo added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,12 +9,12 @@ const Index = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-white to-ronin-gray flex flex-col items-center justify-center p-4">
-      <div className="max-w-4xl w-full text-center space-y-8">
+    <div className="min-h-screen bg-gradient-to-b from-white to-ronin-gray flex flex-col items-center justify-center p-2">
+      <div className="max-w-4xl w-full text-center space-y-4">
         {/* Logo */}
         <div className="w-48 h-48 md:w-64 md:h-64 mx-auto animate-float">
           <img
-            src="/placeholder.svg"
+            src="roninPupNoBg.png"
             alt="RoninPup Logo"
             className="w-full h-full object-contain"
           />
@@ -41,7 +41,7 @@ const Index = () => {
         </div>
 
         {/* Social Icons */}
-        <div className="flex justify-center space-x-6 pt-8">
+        <div className="flex justify-center space-x-6 pt-2">
           <SocialIcon platform="twitter" href="https://twitter.com" />
           <SocialIcon platform="github" href="https://github.com" />
           <SocialIcon platform="facebook" href="https://facebook.com" />
